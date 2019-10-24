518db8fd877c5d8b41e160cbf4f00108126d5d6a
cc_ff <909278799@qq.com>
2019-10-24 09:43:41 +0800

    mybatis的入门案例

--- src/test/java/com/xupt/ff/test/mybatisTest.java
+++ src/test/java/com/xupt/ff/test/mybatisTest.java
@@ -1,12 +1,34 @@
 package com.xupt.ff.test;
 
+import com.xupt.ff.dao.IUserDao;
+import com.xupt.ff.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 public class mybatisTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //1.读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
         //2.创建SqlSessionFactory工厂
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(inputStream);
         //3.使用工厂生产SqlSession对象
+        SqlSession sqlSession = factory.openSession();
         //4.使用SqlSession创建Dao接口的代理对象
+        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
         //5.使用代理对象执行方法
+        List<User> userList = userDao.findAll();
+        for (User user : userList) {
+            System.out.println(user);
+        }
         //6.释放资源
+        sqlSession.close();
+        inputStream.close();
     }
 }
