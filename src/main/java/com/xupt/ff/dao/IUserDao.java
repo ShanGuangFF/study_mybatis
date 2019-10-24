f8990954a7b10ad8cced6356330667d23189a711
cc_ff <909278799@qq.com>
2019-10-24 16:58:17 +0800

    mybatis的入门案例

--- src/main/java/com/xupt/ff/dao/IUserDao.java
+++ src/main/java/com/xupt/ff/dao/IUserDao.java
@@ -1,13 +1,15 @@
 package com.xupt.ff.dao;
 
 import com.xupt.ff.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
-public interface IUserDao {
+public interface   IUserDao {
     /**
      * 查询所有用户
      * @return 包含所用用户的list集合
      */
+    @Select("select * from user")
     List<User> findAll();
 }
--- src/test/java/com/xupt/ff/test/mybatisTest.java
+++ src/test/java/com/xupt/ff/test/mybatisTest.java
@@ -1,18 +1,25 @@
 package com.xupt.ff.test;
 
 import com.xupt.ff.dao.IUserDao;
+import com.xupt.ff.dao.impl.userDaoImpl;
 import com.xupt.ff.domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public class mybatisTest {
-    public static void main(String[] args) throws IOException {
+
+    /**
+     * 使用代理对象，不写dao实现类
+     */
+    @Test
+    public void test1 () throws IOException {
         //1.读取配置文件
         InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
         //2.创建SqlSessionFactory工厂
@@ -29,6 +36,28 @@ public class mybatisTest {
         }
         //6.释放资源
         sqlSession.close();
+        inputStream.close();
+    }
+
+    /**
+     * 使用userDaoImpl，繁琐，没有省去不需要的麻烦
+     * @throws Exception
+     */
+    @Test
+    public void test2() throws Exception{
+        //1.读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.创建SqlSessionFactory工厂
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(inputStream);
+        //3.使用工厂创建userDao实体类
+        IUserDao userDao = new userDaoImpl(factory);
+        //5.使用实体对象执行方法
+        List<User> userList = userDao.findAll();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        //6.释放资源
         inputStream.close();
     }
 }
