ed798007118c960c9c6f49294d6b526d94ec36c6
cc_ff <909278799@qq.com>
2019-10-24 09:32:08 +0800

    mybatis的入门案例

---
+++ src/main/java/com/xupt/ff/dao/IUserDao.java
@@ -0,0 +1,13 @@
+package com.xupt.ff.dao;
+
+import com.xupt.ff.domain.User;
+
+import java.util.List;
+
+public interface IUserDao {
+    /**
+     * 查询所有用户
+     * @return 包含所用用户的list集合
+     */
+    List<User> findAll();
+}
---
+++ src/test/java/com/xupt/ff/test/mybatisTest.java
@@ -0,0 +1,12 @@
+package com.xupt.ff.test;
+
+public class mybatisTest {
+    public static void main(String[] args) {
+        //1.读取配置文件
+        //2.创建SqlSessionFactory工厂
+        //3.使用工厂生产SqlSession对象
+        //4.使用SqlSession创建Dao接口的代理对象
+        //5.使用代理对象执行方法
+        //6.释放资源
+    }
+}
